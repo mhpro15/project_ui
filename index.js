00f152bfa5b6f006f761e835f1cb514be81f27bd
Hung Nguyen <nguyenmanhhungpro15@gmail.com>
2024-12-04 21:14:52 -0500

    Update

--- index.js
+++ index.js
@@ -284,6 +284,11 @@ function submitForm() {
     .then((result) => {
       console.log("Success:", result);
       res = result.prediction.split(",")[0].substring(1);
+      if (res == "np.int64(0)") {
+        res = "Low Crime Solve Rate";
+      } else {
+        res = "High Crime Solve Rate";
+      }
       document.getElementById("result").innerText = res;
     })
     .catch((error) => {
